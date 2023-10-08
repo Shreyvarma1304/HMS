9dd0f0612e30c508088a4a13752fc8c53f4cad01
Shreyvarma1304 <122085080+Shreyvarma1304@users.noreply.github.com>
2023-10-08 18:33:29 +0530

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,6 +1,6 @@
 public class Guest {
-    private String name;
-    private String contactNumber;
-    private String email;
+    private String c_name;
+    private String c_contactNumber;
+    private String c_email;
     // Other attributes and methods for managing a guest
 }
